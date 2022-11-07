674d7905ea2be97827c917916351c13c55d90fc1
Obinna Uko Ofe <obicodeman@yahoo.com>
2022-11-07 14:18:54 +0100

    styling files for reset, normalize, global classes

---
+++ src/pages/AdminDataTable.js
@@ -0,0 +1,13 @@
+import {useEffect, useState} from "react";
+import "./admin-data-table.scss"
+
+
+function AdminDataTable() {
+  return (
+    <div className="admin-data-table">
+      <h1>Data Table </h1>
+    </div>
+  );
+}
+
+export default AdminDataTable;
